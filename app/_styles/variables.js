43523f51c1be8a82e14be564b49f18a7e5bbf8f0
Lindsey Dortch <codewithlinds@gmail.com>
2025-06-11 19:12:32 -0500

    Created Js for variables

---
+++ app/_styles/variables.js
@@ -0,0 +1,14 @@
+export const variables = {
+  $orange: "#fc7935",
+  $green: "#def080",
+  $purple: "#8d9fff",
+  $black: "#141414",
+  $orangeLight: "#fcd8be",
+  $rosca: `"Rosca", system-ui, sans-serif`,
+  $lato: `"Lato", sans-serif`,
+  $bpXl: "75em",
+  $bpLarge: "62.5em",
+  $bpMed: "50em",
+  $bpSmall: "37.5em",
+  $bpXs: "25em"
+};
