eb732ad7366de0eb2b657c3d30a7ea7dfae795f3
Lindsey Dortch <codewithlinds@gmail.com>
2025-06-11 19:38:03 -0500

    Setup header component

---
+++ app/_components/header/Header.jsx
@@ -0,0 +1,12 @@
+import Nav from "@/app/_components/nav/Nav";
+import styles from "./header.module.scss";
+
+function Header() {
+  return (
+    <header>
+      <Nav />
+    </header>
+  );
+}
+
+export default Header;
