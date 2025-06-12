528cfd62fadf35994b57107e7c589fb9cde46b39
Lindsey Dortch <codewithlinds@gmail.com>
2025-06-11 19:30:29 -0500

    Setup base of nav

--- app/_components/nav/Nav.jsx
+++ app/_components/nav/Nav.jsx
@@ -1,7 +1,16 @@
+import LogoBox from "@/app/_components/nav/LogoBox";
 import styles from "./nav.module.scss";
+import NavLinks from "@/app/_components/nav/NavLinks";
+import LoginButtons from "@/app/_components/nav/LoginButtons";
 
 function Nav() {
-  return <nav>Nav</nav>;
+  return (
+    <nav className="nav">
+      <LogoBox />
+      <NavLinks />
+      <LoginButtons />
+    </nav>
+  );
 }
 
 export default Nav;
