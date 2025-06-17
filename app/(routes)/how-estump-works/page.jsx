e870dbb68c3c87141b6bd9ca0efaed81c756886f
Lindsey Dortch <codewithlinds@gmail.com>
2025-06-16 21:02:33 -0500

    Set up how-estump-works page and route

---
+++ app/(routes)/how-estump-works/page.jsx
@@ -0,0 +1,35 @@
+import styles from "./howeStumpWorks.module.scss";
+
+const content = [
+  {
+    image: "",
+    step: "Request a Quote",
+    text: "Start by filling out a quote form, including your stump's measurements. Instructions will guide you through the process, ensuring an accurate estimate. You’ll also pay a 20% deposit that goes directly to eStump to secure your booking."
+  },
+  {
+    image: "",
+    step: "Get Matched with a Stump Specialist",
+    text: "Once your quote is submitted, it gets sent to a pool of trusted stump specialists in your area. They can choose to pick up the job based on your stump’s size and location."
+  },
+  {
+    image: "",
+    step: "Schedule a Time with Your Stump Specialist",
+    text: "When a stump specialist picks up your job, they’ll have 48 hours to schedule a day that works for both parties. You’ll confirm the date and time that suits your schedule."
+  },
+  {
+    image: "",
+    step: "Stump Removal and Payment",
+    text: "On the scheduled day, your stump specialist will come to remove the stump. Once the job is complete, they’ll collect the remaining payment directly from you based on their accepted payment methods."
+  },
+  {
+    image: "",
+    step: "Enjoy Your Stump-Free Yard",
+    text: "With your stump gone, enjoy a clean, clear yard. Your stump specialist will mark the job as complete, and you'll be all set to enjoy your beautiful outdoor space."
+  }
+];
+
+function page() {
+  return <div>ow estump works</div>;
+}
+
+export default page;
